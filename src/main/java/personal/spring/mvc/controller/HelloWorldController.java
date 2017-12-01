68fd8eede1e6716e7481af0f157ee92d54a2e188
echeng <cjs1234567@hotmail.com>
2017-12-01 16:55:44 -0500

    add new files.

--- src/main/java/personal/spring/mvc/controller/HelloWorldController.java
+++ src/main/java/personal/spring/mvc/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package personal.spring.mvc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloWorldController {
+
+    Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
     @RequestMapping("/hello")
     public ModelAndView helloWorld() {
+        LOGGER.error("I am here");
+
         String message = "HELLO SPRING MVC HOW R U";
         return new ModelAndView("hellopage", "message", message);
     }
