42e5cc908c176790b15dc554fa1566f35430f587
echeng <cjs1234567@hotmail.com>
2017-12-05 16:30:26 -0500

    modify comments.

--- src/main/java/personal/spring/mvc/configuration/dispatcher2/DispatcherController.java
+++ src/main/java/personal/spring/mvc/configuration/dispatcher2/DispatcherController.java
@@ -3,9 +3,13 @@ package personal.spring.mvc.configuration.dispatcher2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class DispatcherController {
@@ -16,8 +20,12 @@ public class DispatcherController {
 	Logger LOGGER = LoggerFactory.getLogger(DispatcherController.class);
 	
     @RequestMapping("/hello")
-    public ModelAndView helloWorld() {
+    public ModelAndView helloWorld(HttpServletRequest request) {
 //    	LOGGER.error(sqlReader);
+        for(String s: ((ApplicationContext)RequestContextUtils.findWebApplicationContext(request).getParentBeanFactory()).getBeanDefinitionNames()){
+            LOGGER.error(s);
+        }
+
         String message = sqlReader;
         return new ModelAndView("hellopage", "message", message);
     }
