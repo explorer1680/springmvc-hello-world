a51cf9feb354a52ce3e86c5bc1271d94719d7115
echeng <cjs1234567@hotmail.com>
2017-12-06 14:43:55 -0500

    add url based view resolver test case.

---
+++ src/main/java/personal/spring/mvc/view/urlbased/UrlBasedController.java
@@ -0,0 +1,16 @@
+package personal.spring.mvc.view.urlbased;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UrlBasedController {
+
+    @GetMapping(path = "url")
+    public ModelAndView handlerRequest(){
+
+        ModelAndView model = new ModelAndView("UrlBasedResolverPage");
+        return model;
+    }
+}
